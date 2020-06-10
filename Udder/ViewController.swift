03a921afb92bf78eb06b2bb828c0b8b8d79d7e39
Ichiro Mori <ichiro-mori@morien.com>
2020-06-10 10:05:35 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 森一朗 on 2020/06/10.
+//  Copyright © 2020 IchiroMori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
